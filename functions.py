480a059d2ce7699f60f723f52f4c133e196cb899
magichampz <91732309+magichampz@users.noreply.github.com>
2023-10-16 23:53:28 +0100

    added view matches function and fixes

--- functions.py
+++ functions.py
@@ -6,6 +6,9 @@ import csv
 # code will have to be adjusted for any other csv's obtained of 
 # different formats from different websites
 
+file = open("game-stats-2021-2022.csv","r")
+data = list(csv.reader(file, delimiter=","))
+
 match_df = pd.read_csv("game-stats-2021-2022.csv") # change to target season
 match_df.head()
 
@@ -20,6 +23,10 @@ def checkForm(streak):
     Args:
         streak (list): teams list of results in a list. Each item 
         in the array is either 0,1,2 corresponding to a Win, Draw or Loss
+        
+    Returns:
+        table (dict): the prem table in a dict, no sorting
+        league_table (DataFrame): the prem table as a Pandas DataFrame, sorted by points
     '''
     if len(streak)<5: 
         form = (streak.count(0)/len(streak))*100
@@ -83,10 +90,43 @@ def getLeagueTable(weeks=38):
     league_table = league_table.drop('STREAK',axis =1)
     league_table = league_table.sort_values(by=['PTS'], ascending= False)
     
-    # return league_table
-    return table
+    return table, league_table
 
     
+def view_next_matches(weeks=0):
+    '''
+    To view the matches from a certain week onwards, may be used for training a match predictor algorithm
+    make a nested list, and each child list represents a match and all the relevant data
+    
+    
+    Args:
+        weeks (int): view all matches from the week specified onwards
+    
+    Returns:
+        all_matches (list): nested list containing details of every match in its own list
+        data in child list- home team, away team, home points, away points, home GSA, home GCA, away GSA, away GCA, home form, away form, result
+    '''
+    all_matches = []
+    # weeks = 5 
+
+    for week in range(weeks,39):
+        for match in data[(2+week*10):(2+(week+1)*10)]:
+            l = []
+            l.append(match[3])
+            l.append(match[4])
+            current_table, current_table_df = getLeagueTable(week)
+            l.append(current_table[match[3]][0])
+            l.append(current_table[match[4]][0])
+            l.append(current_table[match[3]][5])
+            l.append(current_table[match[3]][6])
+            l.append(current_table[match[4]][5])
+            l.append(current_table[match[4]][6])
+            l.append(current_table[match[3]][4])
+            l.append(current_table[match[4]][4])
+            l.append(match[7])
+            all_matches.append(l)
+            
+    return all_matches
 
     
 
